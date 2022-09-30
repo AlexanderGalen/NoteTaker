1b5653d6477b7d3812b7a2d9a988bb337c53a149
Alexander Carr <alexanderc93@gmail.com>
2022-09-30 10:29:26 -0700

    add route for posting new notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,8 @@
 const notes = require("express").Router();
 const fs = require("fs");
 const path = require("path");
+const { v4: uuid } = require('uuid');
+
 
 const dbPath = path.join(process.cwd(), "db", "db.json");
 
@@ -19,9 +21,43 @@ notes.get("/", (req, res) => {
 });
 
 notes.post("/", (req, res) => {
-    console.log("post request to notes recieved");
+
     console.log(req.body);
-    res.status(200).send("successfully added note")
+    // generate a unique ID for this note and make an object containing the note data
+    const thisNote = {
+        id: uuid(),
+        title: req.body.title,
+        text: req.body.text,               
+    }
+
+    // read from db to get existing notes as an array
+    fs.readFile(dbPath, "utf8", (err, data) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+
+            let notesList = JSON.parse(data);
+            
+            // add our note to the end of the existing notes
+            notesList.push(thisNote);
+
+            // write our new notes list as a json string
+            fs.writeFile(dbPath, JSON.stringify(notesList), (err) => {
+                if(err) {
+                    console.log(err);
+                }
+                else {
+                    // once we've written our new notes, just send this note's data as a response for the front end
+                    console.log("successfully added note ", thisNote);
+                    res.status(200).json(thisNote);
+                }
+            })
+            
+
+        }
+    }) 
+
 })
 
 // set up post route for notes api
